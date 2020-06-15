42293356706820dd3e236387da04bdd821e35c12
Arnie Serrano <arniekidd@gmail.com>
2020-06-15 01:15:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 function theBeatlesPlay(musicians, instruments){
   var beatlesInstruments=[]
     for(let i=0; i<theBeatlesPlay.length; i++){
-      beatlesInstruments.push(musicians:[i])
+      beatlesInstruments.push(musicians:[i]+" plays "+instruments[i])
     }
   return beatlesInstruments
 }
