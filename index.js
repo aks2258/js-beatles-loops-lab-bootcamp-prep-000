fc593980b0ad102118992a20fe71e01dd1654816
Arnie Serrano <arniekidd@gmail.com>
2020-06-15 01:19:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // add solution here
 function theBeatlesPlay(musicians, instruments){
   var beatlesInstruments=[]
-    for(let i=0; i<theBeatlesPlay.length; i++){
+    for(let i=0; i<musicians.length; i++){
       beatlesInstruments.push(musicians[i] + " plays " + instruments[i])
     }
   return beatlesInstruments
