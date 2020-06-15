72b49436ae02c4573c964fcbb353079563436a96
Arnie Serrano <arniekidd@gmail.com>
2020-06-15 01:19:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // add solution here
-function theBeatlesPlay(musicians, instruments) {
-  var beatlesInstruments = [];
-  for (let i = 0; i < musicians.length; num++) {
-   beatlesInstruments.push(musicians[num] + " plays " + instruments[num]); 
-  }
-  return beatlesInstruments;
+function theBeatlesPlay(musicians, instruments){
+  var beatlesInstruments=[]
+    for(let i=0; i<theBeatlesPlay.length; i++){
+      beatlesInstruments.push(musicians[i] + " plays " + instruments[i])
+    }
+  return beatlesInstruments
 }
