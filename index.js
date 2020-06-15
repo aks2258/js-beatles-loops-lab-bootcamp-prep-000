c97065b4bca84686b12c768ad78859abe4519b40
Arnie Serrano <arniekidd@gmail.com>
2020-06-15 02:02:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function johnLennonFacts(array) {
   var facts = []
   let i = 0
   while (i < array.length) {
-    facts.push(array[i] + "!!!")
+    facts.push(array[i] + "!")
     i++
   }
   return facts;
