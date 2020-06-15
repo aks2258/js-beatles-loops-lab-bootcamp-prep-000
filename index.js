b6b8e0a4d917913646d994ca33ae34c4f9b7dd70
Arnie Serrano <arniekidd@gmail.com>
2020-06-15 01:14:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 // add solution here
 function theBeatlesPlay(musicians, instruments){
   var beatlesInstruments=[]
+    for(let i=0; i<theBeatlesPlay.length; i++){
+      console.log()
+    }
   return beatlesInstruments
 }
